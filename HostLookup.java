ea56f84abee817850eb9b1f082b576e9cbae5c1f
diggerydoo68 <rammerjam64@gmail.com>
2016-01-29 12:26:05 -0700

    Create HostLookup.java
    This file scans for hosts over the internet.

---
+++ HostLookup.java
@@ -0,0 +1,47 @@
+package softwareDev;
+import java.util.Scanner;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class HostLookup {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args)
+	{
+		System.out.println(
+				"Welcome to the IP lookup application.");
+		String host;
+		do
+		{
+			System.out.println("\nEnter a host name: ");
+			host = sc.nextLine();
+			try
+			{
+				InetAddress[] addresses // array of ip addresses
+					= InetAddress.getAllByName(host);
+				for (InetAddress ip : addresses)
+					System.out.println(ip.toString());
+			}
+			catch (UnknownHostException e)
+			{
+				System.out.println("Unknown host.");
+			}
+		} while (doAgain());
+	}
+	
+	private static boolean doAgain()
+	{
+				System.out.println();
+				String s;
+				while (true)
+				{
+					System.out.print("Look up another? "
+							+ "(Y or N) ");
+					s = sc.nextLine();
+					if (s.equalsIgnoreCase("Y"))
+						return true;
+					else if (s.equalsIgnoreCase("N"))
+						return false;
+				}
+			}
+		}
