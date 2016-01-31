a6d1cde60dcce7d5e25577a5c2ce7c3ac3b12a08
Stucki-Kyle <youshouldemailme1@gmail.com>
2016-01-30 22:49:10 -0700

    Create Owner.java

---
+++ Owner.java
@@ -0,0 +1,10 @@
+public class Owner {
+	public static void main(String[] args) {
+		
+		Car Car1 = new Ferrari("Ferrari", "458 Italia", 2012);
+		Car Car2 = new Nissan("Nissan", "GT-R", 2009);
+
+		System.out.println(Car1);
+		System.out.println(Car2);
+	}
+}
