6b21fe8093a5736d57b2d3d6eb1949e3ba684353
diggerydoo68 <rammerjam64@gmail.com>
2016-03-09 16:44:09 -0700

    Create Musician.java

---
+++ RyanRportfolio/Apps/Bands/Musician.java
@@ -0,0 +1,20 @@
+package order;
+
+public class Musician extends Bands{
+protected String description;
+	
+	Musician() {
+		
+	}
+	
+	Musician(String Instrument, String Genre, String description) {
+		super(Instrument, Genre);
+		this.description = description;
+	}
+	
+	//to string is used to pull and print
+	public String toString() {
+		return " " + Instrument + ", " + Genre + ", " + description + " "; 
+	}
+
+}
