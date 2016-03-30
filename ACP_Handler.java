228a7cf3c4ab33fa606a237239071e79aa3ca69e
Stucki-Kyle <youshouldemailme1@gmail.com>
2016-03-29 19:50:02 -0600

    Added files via upload

---
+++ ACP_Handler.java
@@ -0,0 +1,7 @@
+// Author: Kyle Stucki
+
+// Interface used to implement from for our various methods
+public interface ACP_Handler {
+
+	void execute(double number1, double number2);
+}
---
+++ ACP_Multiply.java
@@ -0,0 +1,10 @@
+// Author: Kyle Stucki
+
+public class ACP_Multiply implements ACP_Handler {
+	
+	public void execute(double number1, double number2) {
+		
+		double sum = number1 * number2;
+		System.out.println(sum);
+	}
+}
