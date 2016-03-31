195b6db4d6e6453eb42004ab74c321e8fb9e6863
diggerydoo68 <rammerjam64@gmail.com>
2016-03-31 17:28:37 -0600

    Create MusicianView.java

---
+++ RyanRportfolio/MVC/MusicianView.java
@@ -0,0 +1,7 @@
+public class MusicianView {
+	   public void printMusicianDetails(String musicianName, String musicianRollNo){
+	      System.out.println("Musician: ");
+	      System.out.println("Name: " + musicianName);
+	      System.out.println("Roll No: " + musicianRollNo);
+	   }
+	}
