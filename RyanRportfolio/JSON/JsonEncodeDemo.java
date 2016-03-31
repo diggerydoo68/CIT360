dca107714851322458e1690d809a5b67bef68238
diggerydoo68 <rammerjam64@gmail.com>
2016-03-31 17:19:31 -0600

    Create JsonEncodeDemo.java

---
+++ RyanRportfolio/JSON/JsonEncodeDemo.java
@@ -0,0 +1,17 @@
+package json;
+
+import org.json.simple.JSONObject;
+
+class JsonEncodeDemo {
+
+   public static void main(String[] args){
+      JSONObject obj = new JSONObject(); //Encoding the values in a new object
+
+      obj.put("name", "Neil Peart");
+      obj.put("Ranking", new Integer(100));
+      obj.put("networth", new Double(1000.21));
+      obj.put("is_vip", new Boolean(true));
+
+      System.out.print(obj);
+   }
+}
